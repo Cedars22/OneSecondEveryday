ba8d682414c6fb27b3e8e58e57ff6a2c5de3edf7
Cedars_ <cedars_@Cedars-s-MacBook-Pro.local>
2024-10-30 10:27:54 -0600

    file filter

--- lib/modules/date_selector/pages/add_media_page.dart
+++ lib/modules/date_selector/pages/add_media_page.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:one_second_everyday/modules/date_selector/date_selector_router.dart';
 import 'package:file_picker/file_picker.dart';
 
-class AddMedia extends StatelessWidget {
+class AddMedia extends ConsumerStatefulWidget {
   final DateTime date;
 
   const AddMedia({
@@ -14,52 +15,86 @@ class AddMedia extends StatelessWidget {
   });
 
   @override
+  ConsumerState<AddMedia> createState() => _AddMediaState();
+}
+
+class _AddMediaState extends ConsumerState<AddMedia> {
+  bool selected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    selected = false;
+  }
+
+  @override
   Widget build(BuildContext context) {
+    FilePickerResult? result;
+    File? file;
     return Scaffold(
       appBar: AppBar(),
       body: Center(
         child: ListView(
           children: [
             Text(
-              'Day ${date.toString().split(' ')[0]}',
+              'Day ${widget.date.toString().split(' ')[0]}',
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 24),
             ),
+            if (selected)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: file != null ? Image.file(file!) : Container(),
+              ),
             Card(
               color: Colors.grey[800],
               child: Column(
                 children: [
+                  if (selected)
+                    Container(
+                      height: 200,
+                      // color: Colors.deepPurple,
+                      // child: AssetImage(file!.path),
+                    ),
                   InkWell(
                     onTap: () async {
                       // context.push(
                       //   DateSelectorRouter.selectMediaPage,
                       //   extra: date,
                       // );
-                      FilePickerResult? result =
-                          await FilePicker.platform.pickFiles();
+                      result = await FilePicker.platform.pickFiles(
+                        type: FileType.media,
+                        // allowedExtensions: ['mp4', 'jpg'],
+                      );
                       if (result != null) {
-                        File file = File(result.files.single.path!);
-                        showDialog(
-                            context: context,
-                            builder: (context) {
-                              return AlertDialog(
-                                title: Text('File Selected'),
-                                content: Text(file.path),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: const Text('OK'),
-                                  ),
-                                ],
-                              );
-                            });
+                        file = File(result!.files.single.path!);
+                        AssetImage(file!.path);
+                        setState(() {
+                          selected = true;
+                        });
+                        // selected = true;
+                        // showDialog(
+                        //     context: context,
+                        //     builder: (context) {
+                        //       return AlertDialog(
+                        //         title: Text('File Selected'),
+                        //         content: Text(file.path),
+                        //         actions: [
+                        //           TextButton(
+                        //             onPressed: () {
+                        //               Navigator.pop(context);
+                        //             },
+                        //             child: const Text('OK'),
+                        //           ),
+                        //         ],
+                        //       );
+                        //     });
                       } else {
+                        selected = false;
                         // User canceled the picker
                       }
                     },
-                    child: const Card(
+                    child: Card(
                       margin: EdgeInsets.all(10),
                       child: Column(
                         children: [
@@ -75,6 +110,14 @@ class AddMedia extends StatelessWidget {
                 ],
               ),
             ),
+            // selected
+            // ? const Text('No file selected')
+            // : Text(result!.files.single.path!),
+            // Image.asset(file.path),
+
+            // if (selected){
+            //     AssetImage(file.path),
+            //   }
           ],
         ),
       ),
